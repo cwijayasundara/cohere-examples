ffb6faa342c107244784db4851316b53170c8c45
“cwijayasundara” <cwijayasundara@gmail.com>
2023-08-18 07:35:57 +0100

    first commit

---
+++ keyword-search/keyword-search.py
@@ -0,0 +1,68 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())
+
+import weaviate
+
+auth_config = weaviate.auth.AuthApiKey(
+    api_key=os.environ['WEAVIATE_API_KEY'])
+
+client = weaviate.Client(
+    url='https://cohere-demo.weaviate.network',
+    auth_client_secret=auth_config,
+    additional_headers={
+        "X-Cohere-Api-Key": os.environ['COHERE_API_KEY'],
+    }
+)
+print(client.is_ready())
+
+
+def keyword_search(query,
+                   results_lang='en',
+                   properties=["title", "url", "text"],
+                   num_results=3):
+    where_filter = {
+        "path": ["lang"],
+        "operator": "Equal",
+        "valueString": results_lang
+    }
+
+    response = (
+        client.query.get("Articles", properties)
+        .with_bm25(
+            query=query
+        )
+        .with_where(where_filter)
+        .with_limit(num_results)
+        .do()
+    )
+
+    result = response['data']['Get']['Articles']
+    return result
+
+
+query = "What is the most viewed televised event?"
+keyword_search_results = keyword_search(query)
+print(keyword_search_results)
+
+properties = ["text", "title", "url",
+              "views", "lang"]
+
+
+def print_result(result):
+    """ Print results with colorful formatting """
+    for i, item in enumerate(result):
+        print(f'item {i}')
+        for key in item.keys():
+            print(f"{key}:{item.get(key)}")
+            print()
+        print()
+
+
+print_result(keyword_search_results)
+
+query = "What is the most viewed televised event?"
+keyword_search_results = keyword_search(query, results_lang='de')
+print_result(keyword_search_results)
